168dcd30bb40ddc58359af6981eebaddbde22334
alex <yellowhaojun@gamil.com>
2020-12-27 22:35:19 +0800

    feat(vue-multiple-template): init

---
+++ src/pages/index/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="index">
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'Index',
+  components: {
+  }
+})
+</script>
